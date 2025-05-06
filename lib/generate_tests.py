eca0bdd13aed4b617c2943dd8568d66ed22283f7
Kaparya <kaparyaka.dev@gmail.com>
2025-05-06 21:04:43 +0300

    Move save_file function

--- lib/generate_tests.py
+++ lib/generate_tests.py
@@ -1,3 +1,5 @@
+from lib.save_file import save_file
+
 import os
 
 
@@ -24,27 +26,3 @@ def generate_test_file(file_path: str) -> str:
     # TODO: Write main logic to generate test files
 
     return f"Generated test_file for {file_path}"
-
-
-def save_file(original_file_path: str, content: str):
-    """
-    Save the generated content to a file.
-    Args:
-        original_file_path (str): The path to the original file.
-        content (str): The content to save.
-    """
-
-    split_path = original_file_path.split('/')
-
-    tests_folder = split_path[:-1]
-    tests_folder.append('tests')
-    tests_folder = '/'.join(tests_folder) + '/'
-
-    if not os.path.isdir(tests_folder):
-        os.makedirs(tests_folder)
-
-    test_file_path = 'test_' + '.'.join(split_path[-1].split('.')[:-1]) + '.py'
-    test_file_path = tests_folder + test_file_path
-
-    with open(test_file_path, 'w') as test_file:
-        test_file.write(content)
